e541c888b750984cc1899929206b2f4cf996df66
aaronshi2017 <shi.aaron@gmail.com>
2026-07-26 21:18:09 -0400

    IMS DNN working: ogstun2, Kamailio registrar/proxy, dual PDU session on UE1

--- core/start.sh
+++ core/start.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
+set -e
 ETC=/opt/open5gs/etc/open5gs
 BIN=/opt/open5gs/bin
-cp /config/*.yaml "$ETC"/
 mkdir -p /opt/open5gs/var/log/open5gs
+
+cp /config/*.yaml "$ETC"/
 
 ip tuntap add name ogstun mode tun
 ip addr add 10.45.0.1/16 dev ogstun
 ip link set ogstun up
-iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE || \
-  echo "WARN: iptables unavailable - UE will get IP but no internet"
+iptables -t nat -A POSTROUTING -s 10.45.0.0/16 ! -o ogstun -j MASQUERADE
 
-$BIN/open5gs-nrfd -d & sleep 2
-$BIN/open5gs-scpd -d & sleep 2
-for nf in ausf udm udr pcf bsf nssf; do $BIN/open5gs-${nf}d -d & done
+ip tuntap add name ogstun2 mode tun
+ip addr add 10.46.0.1/16 dev ogstun2
+ip link set ogstun2 up
+iptables -t nat -A POSTROUTING -s 10.46.0.0/16 ! -o ogstun2 -j MASQUERADE
+
+"$BIN"/open5gs-nrfd -d & sleep 2
+"$BIN"/open5gs-scpd -d & sleep 2
+for nf in ausf udm udr pcf bsf nssf; do "$BIN"/open5gs-${nf}d -d & done
 sleep 3
-$BIN/open5gs-upfd -d & sleep 2
-$BIN/open5gs-smfd -d & sleep 2
-$BIN/open5gs-amfd -d &
-echo "=== Open5GS started ==="
+"$BIN"/open5gs-upfd -d & sleep 2
+"$BIN"/open5gs-smfd -d & sleep 2
+"$BIN"/open5gs-amfd -d &
+
+echo "=== Open5GS started: AMF NGAP on 10.33.33.4:38412, UPF GTP-U on 10.33.33.4:2152 ==="
 sleep infinity
